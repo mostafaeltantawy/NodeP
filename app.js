7d43e108bb09c8e644cc2de2056bfec5a2f2c44f
mostafaeltantawy <54724682+mostafaeltantawy@users.noreply.github.com>
2022-12-16 21:49:43 +0200

    app

---
+++ app.js
@@ -0,0 +1,112 @@
+const path = require("path");
+const fs = require("fs");
+const https = require("https");
+const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const csrf = require("csurf");
+const flash = require("connect-flash");
+const multer = require("multer");
+
+const morgan = require("morgan");
+
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.uebbia6.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
+const csrfProtection = csrf();
+const privateKey = fs.readFileSync("server.key");
+const certificate = fs.readFileSync("server.cert");
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toDateString() + file.originalname);
+  },
+});
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+const compression = require("compression");
+const { privateDecrypt } = require("crypto");
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" }
+);
+
+app.use(helmet());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ storage: fileStorage }).single("image"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+// app.use(cookieParser());
+
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use(csrfProtection);
+app.use(flash());
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+app.use(function (req, res, next) {
+  // const token = req.csrfToken();
+  // res.cookie("XSRF-TOKEN", token);
+
+  res.locals.csrfToken = req.csrfToken();
+  // console.log(token);
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  next();
+});
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    console.log("connect");
+
+    https;
+    // .createServer({ key: privateKey, cert: certificate }, app)
+    // .listen(process.env.PORT || 3000);
+    app.listen(process.env.PORT || 3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
